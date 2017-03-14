2b3514a204d72d1bab47daf34e6261e04f38ebbf
tingleshao <cshao@cs.unc.edu>
2017-03-14 11:09:55 -0400

    add a script for calling different steps in hugin

---
+++ automatic.py
@@ -0,0 +1,16 @@
+from subprocess import call
+
+import os
+# generating project file
+os.system("pto_gen -o init.pto *.jpg")
+
+# generating control points
+os.system("cpfind --multirow -o control_pts.pto init.pto")
+
+# pruning control points
+os.system("celeste_standalone -i control_pts.pto -o pruning_pts.pto")
+
+# optimizing positions and geometry
+os.system("autooptimiser -a -l -s -m -o optimized.pto pruning_pts.pto");
+
+# TODO: optional: clean up, remove intmediate files
