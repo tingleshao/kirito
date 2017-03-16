95511b3e4d8c5ea99c2257330ac276d0f7b6e044
tingleshao <cshao@cs.unc.edu>
2017-03-16 16:30:28 -0400

    save update

--- convert_opencv_output.c
+++ convert_opencv_output.c
@@ -1,1 +1,8 @@
 // TODO: write the samething in C as parse_opencv_output.py
+
+int main(int argc, char **argv) {
+    // TODO: implement me
+    for (int i = 0; i < argc; ++i) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+}
--- convert_output_for_hugin.c
+++ convert_output_for_hugin.c
@@ -1,7 +1,8 @@
 // TODO: implement the samething as the one in parse_output_for_hugin.py
 
 void main() {
-// TODO: implement me 
+// TODO: implement me
+
 
 
 }
