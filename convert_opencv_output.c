8d824fc941f2169aa0180d370503573b36ea5fcb
tingleshao <cshao@cs.unc.edu>
2017-03-17 13:57:11 -0400

    update gitignore, add code in convert_opencv_output

--- convert_opencv_output.c
+++ convert_opencv_output.c
@@ -1,8 +1,23 @@
 // TODO: write the samething in C as parse_opencv_output.py
 
+#include <stdio.h>
+
+
+
 int main(int argc, char **argv) {
     // TODO: implement me
     for (int i = 0; i < argc; ++i) {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
+
+    // TODO: read input
+    char const* const filename = argv[1];
+    FILE* file = fopen(filename, "r");
+    char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+    fclose(file);
+    return 0;
 }
