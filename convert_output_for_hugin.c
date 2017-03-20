a3d23bb7aa4bac996848ad4476a065dac7f375d3
tingleshao <cshao@cs.unc.edu>
2017-03-20 15:44:33 -0400

    save update;

--- convert_output_for_hugin.c
+++ convert_output_for_hugin.c
@@ -12,7 +12,6 @@ char** str_split(char* a_str, const char a_delim) {
     char delim[2];
     delim[0] = a_delim;
     delim[1] = 0;
-
     /* Count how many elements will be extracted. */
     while (*tmp) {
         if (a_delim == *tmp) {
@@ -21,16 +20,12 @@ char** str_split(char* a_str, const char a_delim) {
         }
         tmp++;
     }
-
     /* Add space for trailing token. */
     count += last_comma < (a_str + strlen(a_str) - 1);
-
     /* Add space for terminating null string so caller
        knows where the list of returned strings ends. */
     count++;
-
     result = malloc(sizeof(char*) * count);
-
     if (result) {
         size_t idx  = 0;
         char* token = strtok(a_str, delim);
@@ -47,6 +42,7 @@ char** str_split(char* a_str, const char a_delim) {
 }
 
 float ** sort_features(float ** values_lst) {
+  // TODO: implement me
   float important_features[25][4];
   return *important_features;
 }
@@ -72,19 +68,44 @@ int main() {
     int curr_idx = 0;
     char * output = "# control points\n";
 
+    float values_lst[2000][4];
+    int dist_lst[2000];
+    int first_line = 1;
+    int curr_i;
+
+    int adjacent_map[18][3] = {{1, 11, -1}, {0, 2, -1}, {1, 3, -1}, {2, 4, -1},
+        {5, -1, -1}, {4, 6, -1}, {5, 7, -1}, {6, 8, -1}, {7, 9, -1}, {8, 10, -1},
+        {9, 11, -1}, {0, 10, 12}, {11, 13, -1}, {12, 14, -1}, {13, 15, -1},
+        {14, 16, -1}, {15, 17, -1}, {4, 16, -1}};
+
     while (fgets(line, sizeof(line), file)) {
         printf("%s", line);
         char ** tokens;
-        tokens = str_split(line, ' ');
-        int curr_key_0 = atoi(tokens[1]);
-        int curr_key_1 = atoi(tokens[2]);
-        int curr_key[] = {curr_key_0, curr_key_1};
-        int curr_i = curr_idx + 1;
+        tokens = str_split(line, ' '); // split this line with #
+        int curr_key_0;
+        int curr_key_1;
+        int curr_key[2];
 
-        float values_lst[1000][4];
-        int dist_lst[1000];
-
-        while ((strcmp(line, "") != 1) && (line[0] != '#')) {
+        if ((strcmp(line, "") != 1) && ((line[0]) == '#')) {
+            curr_key[0] = atoi(tokens[1]);
+            curr_key[1] = atoi(tokens[2]);
+            // TODO: initialize dist_lst and values_lst
+            // TODO: if not first line, update output string
+            curr_i = 0;
+            if (!first_line) {
+                float ** important_features = sort_features(values_lst);
+                for (int i = 0; i < 1000; i++) { //TODO: the upper limit is the min between 25 and the length of the list of features
+                    char * temp;
+                    sprintf(temp, "c n%d N%d x%f y%f X%f Y%f t0\n",
+                        curr_key[0], curr_key[1], important_features[i][0] * ratio,
+                        important_features[i][1] * ratio, important_features[i][2] * ratio,
+                        important_features[i][3] * ratio);
+                    output = strcat(output, temp);
+                }
+            }
+            first_line = 0;
+        }
+        else if ((strcmp(line, "") != 1) && ((line[0]) != '#')) {
             values_lst[curr_i][0] = atof(tokens[0]);
             values_lst[curr_i][1] = atof(tokens[1]);
             values_lst[curr_i][2] = atof(tokens[2]);
@@ -92,22 +113,7 @@ int main() {
             dist_lst[curr_i] = atoi(tokens[4]);
             curr_i++;
         }
-
-        curr_idx = curr_i;
-        char * output_str;
-        char ** adjacent_map;  // TODO: make this work
-        if (contains(curr_key[1], adjacent_map[curr_key[0]])) {
-            float ** important_features = sort_features(values_lst);
-            for (int i = 0; i < 1000; i++) { //TODO: the upper limit is the min between 25 and the length of the list of features
-                char * temp;
-                sprintf(temp, "c n%d N%d x%f y%f X%f Y%f t0\n",
-                    curr_key[0], curr_key[1], important_features[i][0] * ratio,
-                    important_features[i][1] * ratio, important_features[i][2] * ratio,
-                    important_features[i][3] * ratio);
-                output = strcat(output, temp);
-            }
-        }
     }
-    printf(output);
+    printf("%s", output);
     return 0;
 }
