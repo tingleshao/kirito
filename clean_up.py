417fc9f3cbf7f4cf9dfbabd702a8a2f0521d284a
tingleshao <cshao@cs.unc.edu>
2017-03-23 11:12:16 -0400

    update clean up.

--- clean_up.py
+++ clean_up.py
@@ -1,7 +1,12 @@
 import os
 
+test = True
 
 os.system("rm *.jpg")
 os.system("rm *.txt")
 os.system("rm *.pto")
 os.system("rm -r old_order_images")
+
+if test:
+    os.system("cp ~/dev/kirito/*.py .")
+    os.system("cp ~/dev/kirito/*.jpg .")
