2855dd123aa07d784f1ce9e584d4bc4e0b2ade1e
tingleshao <cshao@cs.unc.edu>
2017-03-30 11:59:00 -0400

    save update

--- parse_output_for_hugin.py
+++ parse_output_for_hugin.py
@@ -19,6 +19,7 @@ adjacent_map = [[1, 8, 9, 10, 11, 12, 13, 14],[0, 2, 7, 8, 9, 13, 14, 15],[1, 3,
                 [0, 1, 2, 7, 9],[0, 1, 8, 10, 11], [0, 9, 11],[0, 9, 10, 12, 13],[0, 11, 13],[0, 1, 11, 12, 14],
                 [0, 1, 2, 13, 15],[1, 2, 3, 14, 16],[2, 3, 4, 15, 17],[3, 4, 16]]
 
+
 def main():
 #    ratio = 920.0 / 670.0
     ratio = 2160.0 / 581.0
@@ -37,7 +38,8 @@ def main():
         while curr_i < len(lines) and len(lines[curr_i]) > 0 and lines[curr_i][0] != '#':
             line = lines[curr_i]
             tokens = line.split(' ')
-            values_lst.append([float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3])])
+        #    values_lst.append([float(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3])])
+            values_lst.append([float(tokens[x]) for x in range(4)])
             dist_lst.append(float(tokens[4]))
             curr_i = curr_i + 1
         curr_idx = curr_i
@@ -83,8 +85,8 @@ def main():
     #         important_features = [values_lst[w] for w in sorted(range(len(values_lst)), key=lambda k: dist_lst[k])]
     #         for i in range(min(len(important_features), 25)):
     #             output_str = output_str + "c n{0} N{1} x{2:.12f} y{3:.12f} X{4:.12f} Y{5:.12f} t0\n".format(curr_key[0], curr_key[1], important_features[i][0] * ratio, important_features[i][1] * ratio, important_features[i][2] * ratio, important_features[i][3] * ratio)
-    with open("parsed_output_for_hugin.txt", "w") as text_file:
-        text_file.write(output_str)
+    with open("parsed_output_for_hugin.txt", 'w') as output_file:
+        text_file.write(output_file)
 
 
 if __name__ == '__main__':
