b809231288841b74e59def2eb3e46a07b7721918
tingleshao <cshao@cs.unc.edu>
2017-04-11 14:57:35 -0400

    save update

--- filter_features_based_on_global_view.py
+++ filter_features_based_on_global_view.py
@@ -69,5 +69,41 @@ def main():
         values_lst = []
         dist_lst = []
         if 0 in curr_key:
+            continue
         else:
+            # if the current matched featrues are also matched pairs in the global map, add this line
             output_str = output_str = line + "\n"
+            while curr_i < len(lines) and len(lines[curr_i]) > 0 and lines[curr_i][0] != '#':
+                line = lines[curr_i]
+                tokens = line.split(' ')
+                that_img_feature_id = int(tokens[6])
+                this_img_feature_id = int(tokens[5])
+                this_image_id = curr_key[0]
+                that_image_id = curr_key[1]
+                # check if the match exist in the global feature match map
+                # 1. check if the feature "this" matches anything in global
+                this_global_matcher = None
+                if this_image_id in global_img_matching_map:
+                    if this_img_feature_id in global_img_matching_map[this_image_id]:
+                        this_global_matcher = global_img_matching_map[this_image_id][this_img_feature_id]
+                # 2. check if the feature "that" matches anything in global
+                that_global_matcher = None
+                if that_image_id in global_img_matching_map:
+                    if that_img_feature_id in global_img_matching_map[that_image_id]:
+                        that_global_matcher = global_img_matching_map[that_image_id][that_img_feature_id]
+                if this_global_matcher != None and that_global_matcher != None:
+                # compare two lists see if there is anything in common
+                    this_matched_list = this_global_matcher[0]
+                    that_matched_list = that_global_matcher[0]
+                    for i in this_matched_list:
+                        if i in that_matched_list:
+                            output_str = output_str + line + "\n"
+                            break
+                curr_i = curr_i + 1
+        curr_idx = curr_i
+    with open("parsed_output.txt", 'w') as output_file:
+        output_file.write(output_str)
+
+
+if __name__ == '__main__':
+    main()
