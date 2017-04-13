ba014104920e1c4be923039e8edbbef4633a6a7c
marbleimplementer <marbleimplementer@gmail.com>
2017-04-13 12:17:29 -0400

    finish script for changing crop factor.

--- update_crop_factor.py
+++ update_crop_factor.py
@@ -4,8 +4,20 @@ import sys
 
 def main():
     filename = sys.argv[1]
+    with open(filename) as input_file:
+        text = input_file.read()
+    lines = text.split('\n')
+    curr_idx = 0
+    output_str = ""
+    while curr_idx < len(lines):
+        line = lines[curr_idx]
+        if line == "#-hugin  cropFactor=1":
+            output_str  = output_str + "#-hugin  cropFactor=7" + '\n'
+        else:
+            output_str = output_str + line + '\n'
+    with open("init2.pto", 'w') as output_file:
+        output_file.write(output_str)
     
-
 
 if __name__ == "__main__":
     main()
