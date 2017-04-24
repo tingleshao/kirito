bf148206bbef3191c15f775979af892db75b011a
tingleshao <cshao@cs.unc.edu>
2017-04-24 13:48:53 -0400

    save update

--- automatic.py
+++ automatic.py
@@ -8,21 +8,21 @@ if len(sys.argv) > 1 and argv[1] == "snap":
     os.system("snap -c sync.cfg")
 
 # generating project file
-os.system("pto_gen -f 14.4 -o init.pto *.jpg")
+#os.system("pto_gen -f 14.4 -o init.pto *.jpg")
 
 #os.system("python3 update_crop_factor.py init.pto")
 # generating control points
-#os.system("cpfind --multirow -o control_pts.pto init2.pto")
+os.system("cpfind --prealigned --kdtreeseconddist=0.45 -o control_pts.pto init2.pto")
 
 # pruning control points
 #os.system("cpclean -o control_pts2.pto control_pts.pto")
 
-#os.system("celeste_standalone -i control_pts2.pto -o pruning_pts.pto")
+os.system("celeste_standalone -i control_pts.pto -o pruning_pts.pto")
 
-#os.system("linefind -o lines.pto pruning_pts.pto")
+os.system("linefind -o lines.pto pruning_pts.pto")
 
 # optimizing positions and geometry
-#os.system("autooptimiser -a -l -s -m -o optimized.pto lines.pto");
+os.system("autooptimiser -a -l -s -m -o optimized.pto lines.pto");
 
 #os.system("python3 HuginMakeSaccadeConfig.py optimized.pto model.json")
     #os.system("hugin_executor --stitching --prefix=prefix optimized.pto")
