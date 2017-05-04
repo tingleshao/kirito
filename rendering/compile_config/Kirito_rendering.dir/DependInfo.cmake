f5eab2d213c2265ad912f132e327c1bc08e6b0ce
tingleshao <cshao@cs.unc.edu>
2017-05-04 12:53:14 -0400

    add more compile config files.

---
+++ rendering/compile_config/Kirito_rendering.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cshao/dev/agt/build/fovea-prefix/src/fovea/FoveaRenderLib/Kirito_rendering.cpp" "/home/cshao/dev/agt/build/fovea-prefix/src/fovea-build/FoveaRenderLib/CMakeFiles/Kirito_rendering.dir/Kirito_rendering.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/cshao/dev/agt/build/fovea-prefix/src/fovea/FoveaRenderLib"
+  "/home/cshao/dev/agt/build/INSTALL/include"
+  "FoveaRenderLib/../FoveaImagePipelineLib"
+  "/home/cshao/dev/agt/build/fovea-prefix/src/fovea/FoveaImagePipelineLib"
+  "/usr/local/cuda-8.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cshao/dev/agt/build/fovea-prefix/src/fovea-build/FoveaRenderLib/CMakeFiles/FoveaEnvisionLib.dir/DependInfo.cmake"
+  "/home/cshao/dev/agt/build/fovea-prefix/src/fovea-build/FoveaImagePipelineLib/CMakeFiles/FoveaImagePipelineLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rendering/compile_config/Kirito_rendering.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kirito_rendering.dir/Kirito_rendering.cpp.o"
+  "Kirito_rendering.pdb"
+  "Kirito_rendering"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kirito_rendering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
