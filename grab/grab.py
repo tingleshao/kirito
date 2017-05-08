217c594dc0a2e35d9773890b7de752748fa7aace
tingleshao <cshao@cs.unc.edu>
2017-05-08 14:24:06 -0400

    add new files in grab

--- grab/grab.py
+++ grab/grab.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys, time
-from pcvform import Ui_PyCamViewer as UI
+#from pcvform import Ui_PyCamViewer as UI
 from PIL import Image
 from PIL.ImageQt import ImageQt
 import MantisPyAPI as mantis
@@ -14,51 +14,81 @@ except:
     from PyQt5 import QtCore, QtWidgets, QtGui
 
 
-newImage = QtCore.pyqtSignal(api.FRAME_METADATA, 'QImage')
-newImage.connect(receiveImage)
-mcamhandle = None
-ipAddress = None
-api.initMCamFrameReceiver(9002, 1)
-api.setMCamFrameCallback(call)
-# --- Set the new mcam callback ---
-api.setNewMCamCallback(newMcam)
+def startServer(start):
+    print("h6")
 
-
-def startServer(self, start):
     if start:
-        ipAddress = "10.0.0.173"
         # --- Connect to the camera ---
-        api.mCamConnect(ipAddress, 9999)
+        print("ipaddress:" + str(ipAddress))
+        mantis.mCamConnect(ipAddress, 9999)
+        print("hi8")
     else:
         # --- Disconnect from the camera ---
-        api.mCamDisconnect(ipAddress, 9999);
+        mantis.mCamDisconnect(ipAddress, 9999);
 
 
-def call(self, meta, jpeg):
+def call(meta, jpeg):
+    print("call get called")
     image = ImageQt(jpeg)
     image = image.convertToFormat(QtGui.QImage.Format_RGB888)
-    self.newImage.emit(meta, image)
+    image.save("test.jpg", 'jpg')
+    newImage.emit(meta, image)
 
 
-def newMcam(self, mcamhandle):
-    if not self.mcamhandle:
-        self.mcamhandle = mcamhandle
+def newMcam(mcamhandle):
+    if not mhandle:
+        mhandle = mcamhandle
 
 
-def grab():
-    if start and mcamhandle:
+def grab(start):
+    print("ipaddress:" + str(ipAddress))
+    mantis.mCamConnect(ipAddress, 9999)
+    print("start" + str(start))
+    print("mhandle" + str(mhandle))
+    print("here")
+    frame = mantis.grabMCamFrame(9999, 1.0)
+    print("her2")
+#    frame.save("test.jpg", 'jpg')
+    mantis.mCamDisconnect(ipAddress, 9999);
+    # --- Start streaming ---
+    #    mantis.startMCamStream(mcamhandle, 9002)
+    # --- Only receive HD ---
+    #    mantis.setMCamStreamFilter(mcamhandle, 9002, mantis.ATL_SCALE_MODE_HD)
+#    elif mhandle:
         # --- Start streaming ---
-        api.startMCamStream(mcamhandle, 9002)
-        # --- Only receive HD ---
-        api.setMCamStreamFilter(mcamhandle, 9002, api.ATL_SCALE_MODE_HD)
-    elif mcamhandle:
-        # --- Start streaming ---
-        api.stopMCamStream(mcamhandle, 9002)
+#        mantis.stopMCamStream(mcamhandle, 9002)
+
+
+def existing():
+    grab(False)
+    startServer(False)
+    time.sleep(0.2)
+    mantis.closeMCamFrameReceiver(9002)
 
 
 def main():
-    grab()
+    print("h4")
+
+#    startServer(True)
+    print("hi5")
+
+    grab(True)
+    existing()
+
+#ewImage = QtCore.pyqtSignal(mantis.FRAME_METADATA, 'QImage')
+#newImage.connect(receiveImage)
+print("hi")
+mhandle = None
+ipAddress = "10.0.0.173"
+print("h2")
+
+mantis.initMCamFrameReceiver(9002, 1)
+#mantis.setMCamFrameCallback(call)
+# --- Set the new mcam callback ---
+#mantis.setNewMCamCallback(newMcam)
 
 
 if __name__ == "__main__":
+    print("hi3")
+
     main()
