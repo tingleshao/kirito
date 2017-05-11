559f1ce95069c943e70969c795f4a4ed0e8da86b
tingleshao <cshao@cs.unc.edu>
2017-05-11 14:22:46 -0400

    add stitiching script

--- automatic.py
+++ automatic.py
@@ -1,7 +1,8 @@
-from subprocess import call
-import sys
 import os
+import sys
+
 import kgui.main as gui
+import stitching
 
 
 # Color coded output helps visualize the information a little better, plus looks cool!
@@ -29,22 +30,4 @@ if len(sys.argv) > 1 and sys.argv[1] == "gui":
     print(string.format(ansi.RED_B))
     gui.main()
 
-# generating project file
-#os.system("pto_gen -f 14.4 -o init.pto *.jpg")
-
-#os.system("python3 update_crop_factor.py init.pto")
-# generating control points
-os.system("cpfind --prealigned --kdtreeseconddist=0.45 -o control_pts.pto init2.pto")
-
-# pruning control points
-#os.system("cpclean -o control_pts2.pto control_pts.pto")
-
-os.system("celeste_standalone -i control_pts.pto -o pruning_pts.pto")
-
-os.system("linefind -o lines.pto pruning_pts.pto")
-
-# optimizing positions and geometry
-os.system("autooptimiser -a -l -s -m -o optimized.pto lines.pto")
-
-#os.system("python3 HuginMakeSaccadeConfig.py optimized.pto model.json")
-    #os.system("hugin_executor --stitching --prefix=prefix optimized.pto")
+stitching.stitching_pure_hugin()
