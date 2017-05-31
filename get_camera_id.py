88a6a78b30d63a392e0d9446b0e831b93e2444f2
tingleshao <cshao@cs.unc.edu>
2017-05-31 15:35:05 -0400

    add a python script for getting camera's ID. have not tested it yet.

---
+++ get_camera_id.py
@@ -0,0 +1,25 @@
+import MantisPyAPI as api
+
+
+def getCameraId(ip, port):
+    cameras = []
+    api.cameraConnect(ip, port)
+    numCameras = api.getNumberOfCameras()
+    api.setNewCameraCallback(lambda x: cameras.append(x))
+    for camera in cameras:
+        print("Found camera with ID "\
+                + str(camera.camID) + " and "\
+                + str(camera.numMCams) + " microcameras")
+    for camera in cameras:
+        api.disconnectCamera(camera)
+    if len(cameras) > 0:
+        return caemras[0].camID
+    return -1
+
+
+def main():
+    print("hi")
+
+
+if __name__ == "__main__":
+    main()
