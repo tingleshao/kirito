50d312f72a1a4cd187569c0b412e381e94c8bd09
tingleshao <cshao@cs.unc.edu>
2017-06-30 20:47:43 -0400

    update matching visualizer

--- rendering/matching_visualizer.py
+++ rendering/matching_visualizer.py
@@ -10,12 +10,20 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
- # TODO: does this work? 
+ # TODO: does this work?
 
 
 def main():
-    return None
+    img1_name = "test_frames/1021700000.jpeg"
+    img2_name = "test_frames/1021700002.jpeg"
+    orb = cv2.ORB()
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
 
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    visualize(img1_name, kp1, img2_namem kp2)
 
 
 def visualize(img1_name, match1, img2_name, match2):
