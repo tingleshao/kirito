c928a2c231a5c4b63a26ca204c1e1a41492af9fa
tingleshao <cshao@cs.unc.edu>
2017-07-05 10:46:29 -0400

    update tests

--- hugin_api/hugin_api.py
+++ hugin_api/hugin_api.py
@@ -2,6 +2,38 @@
 import os
 
 
-def find_features(image_name):
-    os.system("pto_gen {0} -o init.pto".format(image_name))
-    os.system("cpfind --multirow -o control_pts.pto init2.pto")
+def hugin_find_features(image_names):
+    image_names_str = " ".join(image_names)
+    os.system("pto_gen {0} -o temp.pto".format(image_names_str))
+    os.system("cpfind --multirow -o temp_control_pts.pto temp.pto")
+    kp = load_control_points("temp_control_pts.pto")
+    os.system("rm temp.pto")
+    # return key point location and descriptor?
+    return kp
+
+
+def load_control_points(pto_name):
+    with open(pto_name) as pto_file:
+        pto = pto_file.read()
+    pts_list = []
+    lines = pto.split("\n")
+    kp_dict = {}
+    # TODO: this can be rewritten using a filter
+    for line in lines:
+        if len(line) > 1 and line[0] == 'c':
+            pts_list.append(line)
+    for pts in pts_list:
+        tokens = pts.split(" ")
+        cam1_id = int(tokens[1][1:])
+        cam2_id = int(tokens[2][1:])
+        cam1_pt = (float(tokens[3][1:]), float(tokens[4][1:]))
+        cam2_pt = (float(tokens[5][1:]), float(tokens[6][1:]))
+        if not cam1_id in kp_dict.keys():
+            kp_dict[cam_1_id] = [cam1_pt]
+        else:
+            kp_dict[cam_1_id].append(cam1_pt)
+        if not cam2_id in kp_dict.keys():
+            kp_dict[cam_2_id] = [cam2_pt]
+        else:
+            kp_dict[cam_2_id].append(cam2_pt)
+        return kp_dict
--- non_hugin_based_stitching/test_hugin_and_non_hugin_feature_finding.py
+++ non_hugin_based_stitching/test_hugin_and_non_hugin_feature_finding.py
@@ -4,43 +4,40 @@ from numpy.misc import imread
 import cv2
 # TODO; implement this method
 from matching_visualizer import visualize
-# TODO: provide an interface from Hugin to Python to find features
-from hugin_api import hugin_find_features
+from hugin_api.hugin_api import hugin_find_features
 
 
-def test_finding_features_in_hugin_non_hugin_single_image(image_name):
-#    image1_name = # XXX:
+def test_finding_features_image_pair(image_name):
     image1 = imread(image1_name)
     # find features using opencv
     orb = cv2.ORB()
     kp1, des1 = orb.detectAndCompute(img1, None)
-    visualize(kp1, "blue")
     kp2, des2 = hugin_find_features(img1)
-    visualize(kp2, "red")
+    visualize(kp1, "blue", kp2, "red")
 
 
-def test_finding_features_in_hugin_non_hugin_multiple_images():
+def test_finding_features_multiple_image_pairs():
     image_names = #XXX
     for image_name in image_names:
-        test_finding_features_in_hugin_non_hugin_single_image(image_name)
+        test_finding_features_image_pair(image_name)
 
-
-def test_finding_features_and_matching_two_images(image_name1, image_name2):
-#    image1_name = # XXX:
-    image1 = imread(image1_name)
-    # find features using opencv
-    orb = cv2.ORB()
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    visualize(kp1, "blue")
-    kp2, des2 = hugin_find_features(img1)
-    visualize(kp2, "red")
+#
+# def test_finding_features_and_matching_two_images(image_name1, image_name2):
+# #    image1_name = # XXX:
+#     image1 = imread(image1_name)
+#     # find features using opencv
+#     orb = cv2.ORB()
+#     kp1, des1 = orb.detectAndCompute(img1, None)
+#     visualize(kp1, "blue")
+#     kp2, des2 = hugin_find_features(img1)
+#     visualize(kp2, "red")
 
 
 def main():
-    test_finding_features_in_hugin_non_hugin_single_image
+    image1_name = # XXX:
+    image2_name = # XXX:
+    test_finding_features_image_pair([image1_name, image2_name])
+
 
 if __name__ == "__main__":
     main()
-
-#def test_findning_features_and_matching_multiple_images():
-#    image_names = #
